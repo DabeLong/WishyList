b105062ea35b817ec3b7b33e61fbc48bf42408dd
DabeLong <davidmingtianlong@gmail.com>
2016-12-21 23:34:52 -0600

    Remade index.php show that only html is shown when web is visited

--- editWishList.php
+++ editWishList.php
@@ -5,18 +5,39 @@
 	</head>
 	<body>
 
-	<?php
-		session_start();
-		if (array_key_exists("user", $_SESSION)){
-			echo "Hello " . $_SESSION['user'] . "!";
-		} else {
-			// redirect to index
-			header("index.php");
-			exit;
-		}
+		<!-- Check if user is logged in -->
+		<?php
+			session_start();
+			if (array_key_exists("user", $_SESSION)){
+				echo "Hello " . $_SESSION['user'] . "!";
+			} else {
+				// redirect to index
+				header("index.php");
+				exit;
+			}
+		?>
 
+		<!-- Shows current wish as a table -->
+		<table border="black">
+		    <tr><th>Item</th><th>Due Date</th></tr>
+		    <?php
+			    require_once("Includes/db.php");
+			    $wisher_id = WishDB::getInstance()->get_wisher_id_by_name($_SESSION["user"]);
+			    $rows = WishDB::getInstance()->get_wishes_by_wisher_id($wisher_id);
+				foreach ($rows as $row) {
+				    echo "<tr><td>" . htmlentities($row["description"]) . "</td>";
+					echo "<td>" . htmlentities($row["due_date"]) . "</td></tr>\n";
+				}
+		    ?>
+		</table>
 
-	?>
+		<!-- Add new wish -->
+		<form name="addNewWish" action="editWish.php">            
+            <input type="submit" value="Add Wish">
+	    </form>
 
 	</body>
+
+
+
 </html>
--- test.php
+++ test.php
@@ -1,3 +1,18 @@
-<?php
-   echo "Hello world";
+<?php   
+	include("templates/header.htm");   
+
+	// Set the default name 
+	$action = 'index'; 
+	// Specify some disallowed paths 
+	$disallowed_paths = array('header', 'footer'); 
+	if (!empty($_GET['action'])) { 
+	    $tmp_action = basename($_GET['action']); 
+	    // If it's not a disallowed path, and if the file exists, update $action 
+	    if (!in_array($tmp_action, $disallowed_paths) && file_exists("templates/{$tmp_action}.htm")) 
+	        $action = $tmp_action; 
+	} 
+	// Include $action 
+	include("templates/$action.htm"); 
+
+	include("templates/footer.htm");
 ?>
